133171c542e5d3cca32a8bb77dd31e8e85fa5573
Lincoln <lincolny@ngis.com.au>
2022-06-12 15:12:36 +0800

    scaffold project

---
+++ src/App.js
@@ -0,0 +1,38 @@
+const App = () => {
+  return (
+    <div className="categories-conatainer">
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
