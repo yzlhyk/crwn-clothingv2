f3d738b98a71d38f11cfcaa13476559748f07eda
Lincoln <lincolny@ngis.com.au>
2022-06-17 22:20:31 +0800

    87. Add firebase to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component'
+import SignIn from './routes/sign-in/sign-in.component';
 const App = () => {
 
 
@@ -13,6 +14,7 @@ const Shop = () => {
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
