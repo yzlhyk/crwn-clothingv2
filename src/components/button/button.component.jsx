8ead8f231589b29cf03526d468ec69ea22005a25
Lincoln <lincolny@ngis.com.au>
2023-02-27 12:15:56 +0800

    102 customize Button

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,23 @@
+
+import './button.styles.scss';
+
+/* 
+default
+invert
+google sign in
+*/
+
+const BUTTON_TYPES_CLASSES = {
+  google: 'google-sign-in',
+  inverted: 'inverted'
+}
+
+const Button = ({children, buttonType, ...otherProps}) => {
+  return (
+    <button className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`} {...otherProps}>
+      {children}
+    </button>
+  )
+}
+
+export default Button;
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,6 +5,8 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
   displayName: "",
@@ -54,8 +56,9 @@ const SignUpForm = () => {
     setFormFields({ ...formFields, [name]: value });
   };
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -94,7 +97,7 @@ const SignUpForm = () => {
         />
 
 
-        <button type="submit">Submit</button>
+        <Button  type="submit">Submit</Button>
       </form>
     </div>
   );
